47ac151d135964082af1886adbd89545a141f09e
Alejandro Jaez <alejandrojaez@outlook.com>
2020-09-25 18:08:53 -0500

    se movieron archivos

---
+++ Basico/variables.py
@@ -0,0 +1,5 @@
+arr = []
+n = 0
+while True:
+    n+=1
+    arr.append(n)
--- Proyecto Euler/1.py
+++ Proyecto Euler/1.py
@@ -2,6 +2,7 @@
 # Find the sum of all the multiples of 3 or 5 below 1000.
 # Si listamos los numeros naturales debajo de 10 que son multiplos de 3 o 5, tendriamos 3, 5, 6 y 9. La suma de estos multiplos es 23.
 #Encuentra la suma de todos los multiplos de 3 y 5 debajo de 1000.
+
 r = 0
 for n in range(1000):
     if (n%3 == 0 or n%5 == 0):
